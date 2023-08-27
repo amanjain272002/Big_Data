7cca88ef84ec9582aaf55634c804c8418aeec387
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-08-27 10:31:30 +0530

    commit

--- ps1.py
+++ ps1.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
-df = pd.DataFrame('data_folder/Data/census_2011.csv')
-print(df)
+df = pd.read_csv('data_folder/Data/census_2011.csv')
+df = df[['State name','District name','Population','Male','Female','Literate','Male_Literate','Female_Literate','Rural_Households',
+          'Urban_Households','Households','Age_Group_0_29','Age_Group_30_49','Age_Group_50','Age not stated']]
+# print(df)
+df.to_csv('ps1.csv')
+
+
+ 
