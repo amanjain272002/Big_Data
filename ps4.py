d9b2730eb6821b1fcca8cbce809bd2ac60f6df93
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-08-30 16:35:48 +0530

    commit

--- ps4.py
+++ ps4.py
@@ -4,18 +4,24 @@ lst = []
 fl = open('data_folder\Data\Telangana.txt','r')
 for i in fl:
     lst.append(i.strip('\n'))
-print(lst)
+# print(lst)
 fl.close()
-print(len(lst))
+# print(len(lst))
 
 df = pd.read_csv('ps3.csv',index_col=0)
 cnt = 0
-for st in range(0,len(df['State/UT'])):
-    if ((df.loc[st]['State/UT'] == 'Andhra Pradesh') and (df.loc[st]['District'] in lst)):
-        df.replace(df.loc[st]['State/UT'] , 'Telangana')
+df.set_index("District",inplace=True)
+# print(df.head())
+for i in range(0,len(lst)):
+    df.at[lst[i],'State/UT'] = 'Telangana'
             
-         
+df.at['Kargil','State/UT'] = 'Ladakh'
+df.at['Leh(Ladakh)','State/UT'] = 'Ladakh'         
             
 # print(cnt)
+
+df.reset_index(inplace=True)
+# print(df.head())
 # print(df[df['State/UT'] == 'Andhra Pradesh'])
-print(df[df['State/UT'] == 'Telangana'])
+# print(df[df['State/UT'] == 'Telangana'])
+df.to_csv('ps4.csv')
