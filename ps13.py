70eb8c55de4099f9c1e0d03d0dc902a73d9fcbd9
272002 <108273339+amanjain272002@users.noreply.github.com>
2023-09-27 16:42:29 +0530

    commit

---
+++ ps13.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('data_folder\Data\government_hospitals.csv',index_col=0)
+df.dropna(how='any',inplace=True)
+df.reset_index(inplace=True)
+df.rename(columns={'States/UTs':'States/UT','Rural hospitals':'Rural_Government_Hospitals','Unnamed: 2':'Rural_Government_Beds','Urban hospitals':'Urban_Government_Hospitals','Unnamed: 4':'Urban_Government_Beds','As on':'Last_Updated'},inplace=True)
+df.to_csv('ps13.csv')
